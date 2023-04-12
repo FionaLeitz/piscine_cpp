287dcc5de00bb8896e55f298022a7811ae6d0074
Fiona LEITZ <fleitz@e2r6p1.clusters.42paris.fr>
2023-04-12 10:48:00 +0200

    reverse polish notaion

--- cpp09/ex01/main.cpp
+++ cpp09/ex01/main.cpp
@@ -3,23 +3,24 @@
 #include <iostream>
 #include <cstdlib>
 
-void	operation( char c, std::stack<int> operand ) {
+void	operation( char c, std::stack<int> *operand ) {
 
-	// if ( operand.size() < 2 )
-	// 	throw( "Error" );
-	int	a = operand.top();
-	operand.pop();
-	int	b = operand.top();
-	operand.pop();
+	if ( (*operand).size() < 2 )
+		throw std::invalid_argument( "Error" );
+
+	int	a = (*operand).top();
+	(*operand).pop();
+	int	b = (*operand).top();
+	(*operand).pop();
 
 	if ( c == '+' )
-		operand.push( a + b );
+		(*operand).push( a + b );
 	else if ( c == '-' )
-		operand.push( a - b );
+		(*operand).push( b - a );
 	else if ( c == '/' )
-		operand.push( a / b );
+		(*operand).push( b / a );
 	else if ( c == '*' )
-		operand.push( a * b );
+		(*operand).push( a * b );
 
 	return ;
 }
@@ -30,25 +31,43 @@ int	istoken( char c ) {
 	return 0;
 }
 
+int	isnumber( int *count, char *str ) {
+	if ( str[*count] == '-' || str[*count] == '+' ) {
+		if ( isdigit( str[*count + 1] ) != 0 ) {
+			return 1;
+		}
+	}
+	if ( isdigit( str[*count] ) != 0 ) {
+		return 1;
+	}
+	return 0;
+}
+
 void	calcul( char *str, std::stack<int> *operand ) {
 	int	count = 0;
-	char	nbr[2];
-	nbr[1] = '\0';
+	char	nbr[3];
+	nbr[2] = '\0';
 
 	while ( str[count] != '\0' ) {
-		if ( isdigit(str[count]) != 0 ) {
+		if ( isnumber( &count, str ) == 1 ) {
 			nbr[0] = str[count];
+			if ( isdigit( nbr[0] ) == 0 ) {
+				nbr[1] = str[++count];
+			}
 			(*operand).push( atoi( nbr ) );
-			// if ( str[count] != ' ' )
-			// 	throw( "Error" );
+			nbr[1] = '\0';
 		}
 		else if ( istoken( str[count] ) != 0 ) {
-			operation( str[count], *operand );
+			operation( str[count], operand );
 		}
+		else if ( str[count] != ' ' )
+			throw std::invalid_argument( "Error" );
 		count++;
 	}
 
-	std::cout << "TEST: " << (*operand).top() << std::endl; 
+	if ( (*operand).size() != 1 )
+		throw std::invalid_argument( "Error" );
+
 	return ;
 }
 
@@ -60,13 +79,14 @@ int	main( int argc, char **argv ) {
 		return 1;
 	}
 	
-	// try {
-	calcul( argv[1], &operand );
-	// }
-	// catch ( std::exception & e ) {
-	// 	std::cout << e.what() << std::endl;
-	// }
+	try {
+		calcul( argv[1], &operand );
+	}
+	catch ( std::exception & e ) {
+		std::cout << e.what() << std::endl;
+		return 1;
+	}
 
-	std::cout << "FINAL: " << operand.top() << std::endl; 
+	std::cout << operand.top() << std::endl; 
 	return 0;
 }
